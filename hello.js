f8672476e5b5c0d7ff88eb95bb9c6bc3cedf9c8e
Unknown <95ry95ry@gmail.com>
2018-04-25 01:12:15 +0200

    Commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+let test;
+console.log("test =", test, "test is a", typeof test);
+test  = true;
+console.log("test =", test, "test is a", typeof test);
+test += 5;
+console.log("test =", test, "test is a", typeof test);
+test += "test";
+console.log("test =", test, "test is a", typeof test);
---
+++ index.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let app = express();
+let mainRouter = require("./mainRoutes.js");
+let todoRouter = require("./todoRoutes.js");
+
+app.use("/", mainRouter);
+app.use("/todo", todoRouter);
+
+app.listen(3000);
+console.log("Express server running on port 3000");
