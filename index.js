007dfa4dff45113e09fc8aaf9621d1113f919b34
Ryan Verpoort <95ry95ry@gmail.com>
2018-04-26 20:55:15 +0200

    Azure deployment

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use("/", mainRouter);
 app.use("/todo", todoRouter);
 
-app.listen(3000);
+app.listen(process.env.PORT||3000);
 console.log("Express server running on port 3000");
